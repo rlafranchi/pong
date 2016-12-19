0742a2dc649dabc2248e7cbcb58663c2cb1ad2c2
Richard LaFranchi <rlafranchi@icloud.com>
2016-12-18 19:33:25 -0700

    moving ball and randomize directions

--- webpack/src/components/Ball.vue
+++ webpack/src/components/Ball.vue
@@ -6,12 +6,62 @@
 </template>
 
 <script>
+const VALID_SLOPES = [0, 0.0875, 0.2679, 0.5773, 1]
+import _ from 'lodash'
+
 export default {
   name: 'ball',
+  created () {
+    this.serve()
+  },
   data () {
     return {
+      intervalId: null,
       x: 500,
-      y: 400
+      y: 400,
+      direction: {}
+    }
+  },
+  methods: {
+    serve () {
+      this.direction.x = -1
+      this.direction.y = _.sample(VALID_SLOPES)
+      var coinFlip = _.sample([true, false])
+      if (coinFlip) {
+        this.direction.y = -this.direction.y
+      }
+      var that = this
+      this.intervalId = window.setInterval(() => {
+        that.update()
+      }, 1)
+    },
+    update () {
+      var nextY = this.y + this.direction.y
+      var nextX = this.x + this.direction.x
+
+      // upper wall
+      if (nextY >= 465) {
+        this.y = 465
+        this.direction.y = -this.direction.y
+      }
+      // lower wall
+      if (nextY <= 0) {
+        this.y = 0
+        this.direction.y = -this.direction.y
+      }
+      // left paddle
+      if (nextX <= 45) {
+        this.x = 45
+        this.direction.x = 1
+      }
+      // right paddle
+      if (nextX >= 590) {
+        this.x = 590
+        this.direction.x = -1
+      }
+
+      this.x += this.direction.x
+      this.y += this.direction.y
     }
   }
 }
