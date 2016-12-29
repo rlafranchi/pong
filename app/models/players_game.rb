0ffe212810161ec012e8a64386e0bf44b6d87bf1
Richard LaFranchi <rlafranchi@icloud.com>
2016-12-29 08:20:36 -0700

    add test for ensuring only two players per game

--- app/models/players_game.rb
+++ app/models/players_game.rb
@@ -2,4 +2,6 @@ class PlayersGame < ApplicationRecord
   enum position: [:left, :right]
   belongs_to :player
   belongs_to :game
+  validates :game_id, uniqueness: {scope: [:position]}
+  validates :player_id, uniqueness: {scope: [:game_id]}
 end
---
+++ test/models/players_game_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+module Api
+  class PlayersGameTest < ActiveSupport::TestCase
+    def setup
+      @bob = Player.create(name: 'bob')
+      @alice = Player.create(name: 'alice')
+      @game = Game.create
+    end
+  
+    test "should only have one left player" do
+      pg_left = PlayersGame.create(game_id: @game.id, player_id: @bob.id, position: 'left')
+      pg_double = PlayersGame.create(game_id: @game.id, player_id: @alice.id, position: 'left')
+      assert_equal 1, PlayersGame.count
+      assert_not_empty pg_double.errors
+    end
+
+    test "should only have one right player" do
+      pg_right = PlayersGame.create(game_id: @game.id, player_id: @bob.id, position: 'right')
+      pg_double = PlayersGame.create(game_id: @game.id, player_id: @alice.id, position: 'right')
+      assert_equal 1, PlayersGame.count
+      assert_not_empty pg_double.errors
+    end
+  end
+end
