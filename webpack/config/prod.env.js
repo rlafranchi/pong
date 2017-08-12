0485f93eeb7c6f4ad2e1dd2e4e184071ceb816ab
Richard LaFranchi <rlafranchi@icloud.com>
2017-08-12 08:59:10 -0600

    go live

--- webpack/config/prod.env.js
+++ webpack/config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   RAILS_URL: '"https://vue-rails-pong.herokuapp.com"',
-  STRIPE_PK: 'pk_test_fI98pLJ5HlziNWAA56tW5QlT'
+  STRIPE_PK: 'pk_live_iSkA2zFdDI7OWeYM7RZqhEHv'
 }
